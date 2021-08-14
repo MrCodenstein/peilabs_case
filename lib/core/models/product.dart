b82abc145cb9514d1e1b0949b960304308fa20ef
MrCodenstein <60714873+MrCodenstein@users.noreply.github.com>
2021-08-14 16:10:40 +0300

    140821

--- lib/core/models/product.dart
+++ lib/core/models/product.dart
@@ -1,14 +1,7 @@
-// To parse this JSON data, do
-//
-//     final product = productFromJson(jsonString);
-
 import 'dart:convert';
 
 List<Product> productFromJson(String str) =>
     List<Product>.from(json.decode(str).map((x) => Product.fromJson(x)));
-
-String productToJson(List<Product> data) =>
-    json.encode(List<dynamic>.from(data.map((x) => x.toJson())));
 
 class Product {
   Product({
@@ -29,12 +22,4 @@ class Product {
         picture: json["picture"],
         name: json["name"],
       );
-
-  Map<String, dynamic> toJson() => {
-        "id": id,
-        // ignore: unnecessary_null_comparison
-        "parentId": parentId == null ? -1 : parentId,
-        "picture": picture,
-        "name": name,
-      };
 }
